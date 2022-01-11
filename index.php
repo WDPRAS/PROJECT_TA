75d340765272300c6d5bf34a2559c15887ae3d9c
WDPRAS <94670322+WDPRAS@users.noreply.github.com>
2022-01-11 15:17:19 +0700

    memperbarui navbar

--- index.php
+++ index.php
@@ -13,20 +13,24 @@ require 'functions.php';
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">PRESENSI</a>
+  <a class="navbar-brand" href="#">
+  <img src="img/centang.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+  ABSENSI
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">DATA SISWA <span class="sr-only">(current)</span></a>
+    <li class="nav-item">
+        <a class="nav-link" href="absen.php">ABSEN</a>
+      </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">DATA SISWA </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="datapresensi.php">DATA PRESENSI</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="absen.php">ABSEN</a>
       </li>
     </ul>
   </div>
