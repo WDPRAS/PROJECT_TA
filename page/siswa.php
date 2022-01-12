865d3cc5d0dc203a7a0cfe688642a65228005720
SamArif <samarif085@gmail.com>
2022-01-12 11:48:10 +0700

    mengubah require dan icon pada button

--- page/siswa.php
+++ page/siswa.php
@@ -1,6 +1,10 @@
 <?php
-require 'functions.php';
+
+require 'query/functions.php';
+require 'query/call_tbl.php';
+
 ?>
+
 
 <!doctype html>
 <html lang="en">
@@ -10,44 +14,22 @@ require 'functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.7.2/font/bootstrap-icons.min.css"
+        integrity="sha512-1fPmaHba3v4A7PaUsComSM4TBsrrRGs+/fv0vrzafQ+Rw+siILTiJa0NtFfvGeyY5E182SDTaF5PqP+XOHgJag=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>PRESENSI</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-            <img src="img/centang.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
-            ABSENSI
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="absen.php">ABSEN</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">DATA SISWA </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="datapresensi.php">DATA PRESENSI</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
     <div class="row">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
                         <h4 class="card-title">Data Siswa</h4>
-                        <button class="btn btn-primary btn-sm ml-auto" data-toggle="modal" data-target="#addRowModal">
-                            <i class="fa fa-plus"></i>
+                        <button class="btn btn-success btn-sm ml-auto" data-toggle="modal" data-target="#addRowModal">
+                            <i class="bi bi-plus-square"></i>
                         </button>
                     </div>
                 </div>
@@ -68,9 +50,8 @@ require 'functions.php';
                             <tbody>
                                 <tr>
                                     <?php $i=1; ?>
-                                    <?php foreach ($result as $row) : ?>
+                                    <?php foreach ($siswa as $row) : ?>
                                     <td><?= $i; ?></td>
-                                    <td><?= $row["id_siswa"]; ?></td>
                                     <td><?= $row["nama"]; ?></td>
                                     <td><?= $row["asal_sekolah"]; ?></td>
                                     <td><?= $row["nomor"]; ?></td>
@@ -78,19 +59,19 @@ require 'functions.php';
                                     <td class="aksi">
                                         <div class="d-flex align-items-center">
                                             <a href=" " class="btn btn-warning btn-sm m-0  " data-toggle="modal"
-                                                data-target="#EditModal<?= $row["nis"]; ?>">
+                                                data-target="#EditModal<?= $row["id_siswa"]; ?>">
                                                 <span class="icon text-white-70">
-                                                    <i class="fas fa-edit"></i>
+                                                    <i class="bi bi-pen-fill"></i>
                                                 </span>
                                             </a>
                                             <a href=" " class="btn btn-danger  btn-sm m-3" data-toggle="modal"
-                                                data-target="#hapusm<?= $row["nis"]; ?>">
+                                                data-target="#hapusm<?= $row["id_siswa"]; ?>">
                                                 <span class="icon text-white-70">
-                                                    <i class="fas fa-trash-alt"></i>
+                                                    <i class="bi bi-trash-fill"></i>
                                                 </span>
                                             </a>
                                             <!-- Edit modal -->
-                                            <div class="modal fade" id="EditModal<?= $row["nis"]; ?>" tabindex="-1"
+                                            <div class="modal fade" id="EditModal<?= $row["id_siswa"]; ?>" tabindex="-1"
                                                 role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
@@ -187,9 +168,6 @@ require 'functions.php';
         </div>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-        </script>
 
 </body>
 
